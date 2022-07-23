0465b3c7905949eccbafcf7c8db021aea7ffc6d8
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-07-23 18:52:20 -0300

    feat(graphql): create resolvers

--- server/src/graphql/apolloServer.ts
+++ server/src/graphql/apolloServer.ts
@@ -2,10 +2,12 @@ import { ApolloServer } from 'apollo-server-express';
 
 import { typeDefs } from './typeDefs';
 import { apolloConfig } from '../config/apollo';
+import { resolvers } from '../resolvers';
 
 const getApolloServer = () =>
   new ApolloServer({
     typeDefs,
+    resolvers,
     ...apolloConfig,
   });
 
