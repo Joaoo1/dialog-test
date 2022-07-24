3cb100c69592bfc20095deaa56f45e86a0ba7e81
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-07-24 14:46:07 -0300

    feat(web): add and setup apollo client

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
+import { ApolloProvider } from '@apollo/client';
+
+import { apolloClient } from './graphql/apolloClient';
 import { Router } from './routes';
 import GlobalStyles from './styles/global';
 
 export const App = () => (
-  <>
+  <ApolloProvider client={apolloClient}>
     <Router />
     <GlobalStyles />
-  </>
+  </ApolloProvider>
 );
