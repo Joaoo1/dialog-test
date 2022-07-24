d528cc01b0c35573d958895925afdeed47ef445d
João Vitor da Silva <silva.joao1@outlook.com.br>
2022-07-24 15:16:06 -0300

    feat(server): change cors origin to * on development

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,6 +1,8 @@
 import { CorsOptions } from 'cors';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 export const corsOptions: CorsOptions = {
   maxAge: 86400,
-  origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+  origin: isDevelopment ? '*' : [''],
 };
