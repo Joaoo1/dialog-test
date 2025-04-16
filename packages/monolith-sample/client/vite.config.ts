d3737b5c461a63672b141fb9f31085f42b07ec48
João <silva.joao1@outlook.com.br>
2025-04-15 23:31:00 -0300

    build: setup docker for monolith-sample client and server

--- packages/monolith-sample/client/vite.config.ts
+++ packages/monolith-sample/client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		port: 3000,
+		host: "0.0.0.0",
+		watch: {
+			usePolling: true,
+			interval: 500,
+		},
+	},
+});
