fc841273722925f623d316fec6a8b8fd7b0ea811
João <silva.joao1@outlook.com.br>
2025-04-16 16:23:37 -0300

    feat(client): setup react toastify

--- packages/monolith-sample/client/src/App.tsx
+++ packages/monolith-sample/client/src/App.tsx
@@ -2,23 +2,30 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router";
+import { ToastContainer } from "react-toastify";
 import { ProfilePage } from "./pages/ProfilePage";
 import { TimelinePage } from "./pages/TimelinePage";
 import theme from "./styles/theme";
+import { toastStyles } from "./styles/toast";
 
 const queryClient = new QueryClient();
 
 export const App: React.FC = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
+			<ToastContainer
+				theme="dark"
+				autoClose={3000}
+				position="bottom-center"
+				toastStyle={toastStyles}
+			/>
+
 			<ChakraProvider theme={theme}>
 				<Router>
-					<div className="App">
-						<Routes>
-							<Route path="/profile" element={<ProfilePage />} />
-							<Route path="/" element={<TimelinePage />} />
-						</Routes>
-					</div>
+					<Routes>
+						<Route path="/profile" element={<ProfilePage />} />
+						<Route path="/" element={<TimelinePage />} />
+					</Routes>
 				</Router>
 			</ChakraProvider>
 		</QueryClientProvider>
