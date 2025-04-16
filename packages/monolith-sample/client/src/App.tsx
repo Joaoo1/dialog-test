b2911ad77e651daad64ac6ee3bcb33b917f0c252
João <silva.joao1@outlook.com.br>
2025-04-16 16:20:52 -0300

    feat(client): setup React Query

--- packages/monolith-sample/client/src/App.tsx
+++ packages/monolith-sample/client/src/App.tsx
@@ -1,21 +1,26 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router";
 import { ProfilePage } from "./pages/ProfilePage";
 import { TimelinePage } from "./pages/TimelinePage";
 import theme from "./styles/theme";
 
+const queryClient = new QueryClient();
+
 export const App: React.FC = () => {
 	return (
-		<ChakraProvider theme={theme}>
-			<Router>
-				<div className="App">
-					<Routes>
-						<Route path="/profile" element={<ProfilePage />} />
-						<Route path="/" element={<TimelinePage />} />
-					</Routes>
-				</div>
-			</Router>
-		</ChakraProvider>
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider theme={theme}>
+				<Router>
+					<div className="App">
+						<Routes>
+							<Route path="/profile" element={<ProfilePage />} />
+							<Route path="/" element={<TimelinePage />} />
+						</Routes>
+					</div>
+				</Router>
+			</ChakraProvider>
+		</QueryClientProvider>
 	);
 };
